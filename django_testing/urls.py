780ade9c84e9cec649e89b3f29048ea4622f8be6
Eduard Tatarnikov <108517865+Edmaroff@users.noreply.github.com>
2023-04-19 17:20:28 +0800

    changed url coursess11

--- django_testing/urls.py
+++ django_testing/urls.py
@@ -20,7 +20,7 @@ from rest_framework.routers import DefaultRouter
 from students.views import CoursesViewSet
 
 router = DefaultRouter()
-router.register("courses", CoursesViewSet, basename="courses")
+router.register("coursess11", CoursesViewSet, basename="courses")
 
 
 urlpatterns = [
